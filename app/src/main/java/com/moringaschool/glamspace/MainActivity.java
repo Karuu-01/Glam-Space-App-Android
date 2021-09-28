5f5ba82e51ea774cfee7895393fed7f198d59d45
saretoduncan <saretoduncan@gmail.com>
2021-09-28 19:23:10 +0300

    successfully consume unsplash api

--- app/src/main/java/com/moringaschool/glamspace/MainActivity.java
+++ app/src/main/java/com/moringaschool/glamspace/MainActivity.java
@@ -4,27 +4,50 @@ import static com.moringaschool.glamspace.Constants.UNSPLASH_API_KEY;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.moringaschool.glamspace.adapters.ImagesAdapter;
 import com.moringaschool.glamspace.models.ImageSearch;
+import com.moringaschool.glamspace.models.Result;
 import com.moringaschool.glamspace.network.UnsplashClient;
 import com.moringaschool.glamspace.network.UnsplashPhotosApi;
 
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+private ImagesAdapter adapter;
+@SuppressLint("NonConstantResourceId")
+@BindView(R.id.recyclerView1) RecyclerView recyclerView;
+@BindView(R.id.tvpleaseWait) TextView pleaseWaitText;
+@BindView(R.id.progress_circular) ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
@@ -50,12 +73,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void fetchImages(String query) {
+        hideRecyclerView();//hide recycler
         UnsplashPhotosApi client = UnsplashClient.getClient();
         Call<ImageSearch> call = client.getImages(UNSPLASH_API_KEY,query,"1","30");
+        showProgressBar();//show progressbar
         call.enqueue(new Callback<ImageSearch>() {
             @Override
             public void onResponse(Call<ImageSearch> call, Response<ImageSearch> response) {
 
+            if(response.isSuccessful()){
+
+                assert response.body() != null;
+                List<Result> results = response.body().getResults();
+                adapter = new ImagesAdapter(MainActivity.this, results);
+                recyclerView.setAdapter(adapter);
+                RecyclerView.LayoutManager layoutManager= new GridLayoutManager(MainActivity.this,2);
+                recyclerView.setLayoutManager(layoutManager);
+                recyclerView.setHasFixedSize(true);
+                hideProgressBar();//hide progressbar
+                showRecycler();//show recyclerview
+
+            }
             }
 
             @Override
@@ -64,4 +102,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    public void showProgressBar(){//show progressbar
+        progressBar.setVisibility(View.VISIBLE);
+        pleaseWaitText.setVisibility(View.VISIBLE);
+    }
+    public void hideProgressBar(){ // hide progressbar
+        progressBar.setVisibility(View.GONE);
+        pleaseWaitText.setVisibility(View.GONE);
+    }
+    public void hideRecyclerView(){// hide recyclerview
+        recyclerView.setVisibility(View.GONE);
+    }
+    public void showRecycler(){ // show recyclerview
+        recyclerView.setVisibility(View.VISIBLE);
+    }
+
 }
