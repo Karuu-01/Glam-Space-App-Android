efc92ab57815c50789a4a052380c1d7e88198a63
karuu-01 <brian.muchori@student.moringaschool.com>
2021-09-28 12:52:20 +0300

    add activities

---
+++ app/src/main/java/com/moringaschool/glamspace/ui/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.moringaschool.glamspace.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.glamspace.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
